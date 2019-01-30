7242d3ea3a272cb4087cec25953b63944bbdd350
Marco Poli <polimarco1@gmail.com>
2019-01-30 08:59:14 -0300

    Problem 3 is done.

--- problem_003_largest_prime_factor.py
+++ problem_003_largest_prime_factor.py
@@ -1,24 +1,29 @@
 """Davis, January 9th 2019
 Author: Marco Poli
-This program returns the largest prime factor for a given number.
+This program returns the largest prime factor for a given integer.
 """
 from math import sqrt
 
 def is_it_prime(num):
     """This function returns True if a integer is prime."""
-    if (num != 2 and num % 2 == 0) or num <2:
+    if (num != 2 and num % 2 == 0) or num < 2:
         return False
-    aux = num - 2
+
+    aux = int(sqrt(num))
+
+    if aux % 2 == 0:
+        aux -= 1
+
     while aux > 2:
         if num % aux == 0:
             return False
         aux -= 2
+
     return True
-        
 
 def largest_prime_factor(num):
-    """This fucntion returns the largest prime factor 
-    for a given number.
+    """This function returns the largest prime factor
+    for a given integer.
     """
     aux = int(sqrt(num))
 
@@ -27,13 +32,3 @@ def largest_prime_factor(num):
             return aux
         else:
             aux -= 1
-
-    
-
-#print(largest_prime_factor(600851475143))
-#print(largest_prime_factor(13195))
-
-for i in range(1,1101,1):
-    print("For number {} the largest prime is {}".format(i, \
-            largest_prime_factor(i)))
-###    print("For number {} the largest prime is {}".format(i, i))
